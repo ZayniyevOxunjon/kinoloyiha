c85d1feb1c9bed791b46b40944841f7543c41fc1
ZayniyevOxunjon <zayniyevoxunjon19@gmail.com>
2025-04-20 11:18:38 +0500

    added urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, CountryViewSet, GenreViewSet, FilmViewSet
 
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r'countries', CountryViewSet, basename='countries')
+router.register(r'genres', GenreViewSet, basename='genres')
+router.register(r'films', FilmViewSet, basename='films')
 
 urlpatterns = [
-
+path('', include(router.urls)),
 ]
 
